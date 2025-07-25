44d335f02fe100c4e5515c1ea4f0a4becd49f3f0
Malek Bsaissa <91043333+AtfastrSlushyMaker@users.noreply.github.com>
2025-07-25 19:29:32 +0100

    feat: Update dependencies and enhance CreateCourseModal styles for improved button interactions

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './tailwind.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'antd/dist/reset.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
